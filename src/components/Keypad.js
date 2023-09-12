8eba9ddc526964956cc56d4b24a3c574065864be
ann gamal <anngamal14@gmail.com>
2023-09-12 10:54:35 -0400

    complabs

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange(event) {
+        console.log("Entering password")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" name="password" placeholder="Entering password" onChange={handleChange} />
+        </div>
     )
 }
 
 export default Keypad;
+
+//<input type="text" name="username" placeholder="Enter username..." />
